a651822afc54f6766fda21e474a9cc05a3def3ad
Kevin Wonus <kevin@wonus.com>
2025-01-26 23:39:20 -0500

    Simplify installation and deployment of the data manager; also display more info on splash screen

--- SplashScreen.xaml.cs
+++ SplashScreen.xaml.cs
@@ -1,25 +1,11 @@
-﻿using Microsoft.AspNetCore.Hosting.Server;
-using System.Text;
+﻿using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using AVAPI;
 using System.Windows.Threading;
 using System.Runtime.InteropServices;
-using Pinshot.Blue;
-using Blueprint.Blue;
-using AVXFramework;
 using System.IO;
-using System.Collections;
-using System.Linq.Expressions;
-using System.Runtime.Intrinsics.X86;
+using AVXFramework;
+using Pinshot.Blue;
+using AVAPI;
 
 namespace AV_Data_Manager
 {
@@ -34,7 +20,6 @@ namespace AV_Data_Manager
 
         [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr FindWindow(string? lpClassName, string? lpWindowName);
-
 
         [DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
@@ -104,83 +89,31 @@ namespace AV_Data_Manager
             this.Timer.Start();
 
             this.InitializeWebServer();
-            this.Button_Start_Click();
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.running)
+            {
+                var task = this.Server.Stop();
+                task.Wait(2500);
+            }
+        }
+
         private void InitializeWebServer()
         {
             Server = new API();
-
-#if USE_ALL_FEATURES_OF_SAMPLE_APPLICATION
-            // Optionally Intercept Request Start/Completed for logging or UI
-            Server.OnRequestCompleted = (ctx, ts) =>
-            {
-                // Request comes in on non-ui thread!
-                Dispatcher.Invoke(() =>
-                {
-                    var method = ctx.Request.Method.PadRight(8);
-                    var path = ctx.Request.Path.ToString();
-                    var query = ctx.Request.QueryString.ToString();
-                    if (!string.IsNullOrEmpty(query))
-                        path += query;
-                    var status = ctx.Response.StatusCode;
-
-
-                    var text = method + path.PadRight(94) +
-                               " (" + status + ") " +
-                               ts.TotalMilliseconds.ToString("n3") + "ms";
-                    double lines = Math.Floor((RequestMessages.ActualHeight) / (RequestMessages.LineHeight));
-                    Model.AddRequestLine(text, Convert.ToInt32(lines - 1));
-                    Model.RequestCount++;
-                });
-            };
-#endif
+            var server = this.Server.LaunchAsync();
+            SplashScreen.FireAndForget(server);
+            this.running = true;
         }
+
         public static void FireAndForget(Task task)
         {
             task.ContinueWith(tsk => tsk.Exception,
                 TaskContinuationOptions.OnlyOnFaulted);
         }
 
-        private async void Button_Start_Click()
-        {
-#if USE_ALL_FEATURES_OF_SAMPLE_APPLICATION
-            Statusbar.ShowStatusSuccess("Server started.");
-#endif
-            var server = this.Server.LaunchAsync();
-            SplashScreen.FireAndForget(server);
-            this.running = true;
-            this.StartStop.Content = "Stop";
-
-#if USE_ALL_FEATURES_OF_SAMPLE_APPLICATION
-            Model.RequestText = "*** Web Server started.";
-            Model.ServerStatus = "server is running";
-#endif
-        }
-
-        private async void Button_Stop_Click()
-        {
-            await Server.Stop();
-            this.running = false;
-            this.StartStop.Content = "Start";
-
-#if USE_ALL_FEATURES_OF_SAMPLE_APPLICATION
-            Statusbar.ShowStatusSuccess("Server stopped.");
-            Model.ServerStatus = "server is stopped";
-            Model.RequestText = "*** Web Server is stopped. Click Start Server to run.";
-#endif
-        }
-
-        private void StartStop_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.running)
-            {
-                Button_Stop_Click();
-            }
-            else
-            {
-                Button_Start_Click();
-            }
-        }
         private void Timer_Tick(object sender, EventArgs e)
         {
             this.Timer.Interval = new TimeSpan(hours: 24, minutes: 0, seconds: 0);
